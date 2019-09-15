eddb146dd5c86c4653f55ec2491a5eb2a636f781
Diana Tofan <diana_tofan@ymail.com>
2019-09-16 01:02:11 +0200

    Updates

---
+++ precache-manifest.f8468783065e4ad0a4f92f7efce9cd4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ca96e2e65e60168c34c0553e95fc7db",
+    "url": "/githubUserSearch/index.html"
+  },
+  {
+    "revision": "97ad3dcca39102b04092",
+    "url": "/githubUserSearch/static/css/main.bce1c32c.chunk.css"
+  },
+  {
+    "revision": "499d092111f0ad353a63",
+    "url": "/githubUserSearch/static/js/2.b7eb465d.chunk.js"
+  },
+  {
+    "revision": "97ad3dcca39102b04092",
+    "url": "/githubUserSearch/static/js/main.9e5a67c1.chunk.js"
+  },
+  {
+    "revision": "e9e4ac0d81f810ea004f",
+    "url": "/githubUserSearch/static/js/runtime~main.0aae2f13.js"
+  },
+  {
+    "revision": "138e3449a694c9e28c78a2743ee592a4",
+    "url": "/githubUserSearch/static/media/logo.138e3449.svg"
+  }
+]);
